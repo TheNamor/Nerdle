42ba274d86f47bc903e5b772632c9be4b884b6d1
TheNamor <thenamor2@gmail.com>
2026-08-19 23:48:02 -0400

    fix colors, move to pwa generated manifest, new build

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,33 @@ module.exports = defineConfig({
         workboxOptions: {
             skipWaiting : true,
             maximumFileSizeToCacheInBytes: 50000000
-        }
+        },
+        manifestPath: 'manifest.json',
+        themeColor: "#0f1312",
+        msTileColor: "#0f1312",
+        manifestOptions: {
+          background_color: "#0f1312",
+          theme_color: "#4caf50",
+          start_url: "/Nerdle/",
+          id: "/Nerdle/",
+          display: "standalone",
+          name: "Nerdle",
+          short_name: "Nerdle",
+          description: "A daily numbers game using numerical properties.",
+          icons: [
+            {
+                src: "/web-app-manifest-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "maskable"
+            },
+            {
+                src: "/web-app-manifest-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "maskable"
+            }
+          ]
+        },
     },
 })
